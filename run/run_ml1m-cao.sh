43374063c24287cb5d4056ff55720835ea24102d
juarez.sacenti <juarezsacenti@gmail.com>
2020-02-01 16:36:33 -0300

    rework 2 runs

--- run/run_ml1m-cao.sh
+++ run/run_ml1m-cao.sh
@@ -22,6 +22,7 @@ then
     CUDA_VISIBLE_DEVICES=0 nohup python run_knowledge_representation.py -data_path ~/git/datasets/ml1m-cao/ -log_path ~/git/results/ml1m-cao/ -kg_test_files valid.dat:test.dat -l2_lambda 0 -model_type transe -nohas_visualization -dataset ml1m -batch_size 256 -embedding_size 100 -learning_rate 0.001 -topn 10 -seed 3 -eval_interval_steps 9150 -training_steps 915000 -early_stopping_steps_to_wait 45750 -optimizer_type Adam -L1_flag -norm_lambda 1 -kg_lambda 1 &
     BACK_PID=$!
     wait $BACK_PID
+    mv ../results/ml1m-cao/ml1m-transe-*.ckpt_final ../results/ml1m-cao/ml1m-transe-pretrained.ckpt
 fi
 
 #[TRANSR]
@@ -38,14 +39,7 @@ then
     CUDA_VISIBLE_DEVICES=0 nohup python run_knowledge_representation.py -data_path ~/git/datasets/ml1m-cao/ -log_path ~/git/results/ml1m-cao/ -kg_test_files valid.dat:test.dat -l2_lambda 0 -model_type transh -nohas_visualization -dataset ml1m -batch_size 256 -embedding_size 100 -learning_rate 0.001 -topn 10 -seed 3 -eval_interval_steps 9150 -training_steps 915000 -early_stopping_steps_to_wait 45750 -optimizer_type Adam -L1_flag -norm_lambda 1 -kg_lambda 1 -load_ckpt_file ~/git/results/ml1m-cao/ml1m-transe-pretrained1.ckpt &
     BACK_PID=$!
     wait $BACK_PID
-fi
-
-#TransUP
-if no_exist "../results/ml1m-cao/ml1m-transup-*.log"
-then
-    CUDA_VISIBLE_DEVICES=0 nohup python run_item_recommendation.py -data_path ~/git/datasets/ml1m-cao/ -log_path ~/git/results/ml1m-cao/ -rec_test_files valid.dat:test.dat -l2_lambda 1e-5 -negtive_samples 1 -model_type transup -nohas_visualization -dataset ml1m -batch_size 256 -embedding_size 100 -learning_rate 0.1 -topn 10 -seed 3 -eval_interval_steps 500 -training_steps 50000 -early_stopping_steps_to_wait 2500 -optimizer_type Adagrad -L1_flag -num_preferences 20 -nouse_st_gumbel -load_ckpt_file ~/git/results/ml1m-cao/ml1m-bprmf-pretrained1.ckpt &
-    BACK_PID=$!
-    wait $BACK_PID
+    mv ../results/ml1m-cao/ml1m-transh-*.ckpt_final ../results/ml1m-cao/ml1m-transh-pretrained.ckpt
 fi
 
 #FM - Steffen Rendle. 2010. Factorization machines.
@@ -62,6 +56,16 @@ then
     CUDA_VISIBLE_DEVICES=1 nohup python run_item_recommendation.py -data_path ~/git/datasets/ml1m-cao/ -log_path ~/git/results/ml1m-cao/ -rec_test_files valid.dat:test.dat -l2_lambda 1e-5 -negtive_samples 1 -model_type bprmf -nohas_visualization -dataset ml1m -batch_size 256 -embedding_size 100 -learning_rate 0.1 -topn 10 -seed 3 -eval_interval_steps 5000 -training_steps 500000 -early_stopping_steps_to_wait 25000 -optimizer_type Adagrad &
     BACK_PID=$!
     wait $BACK_PID
+    mv ../results/ml1m-cao/ml1m-bprmf-*.ckpt_final ../results/ml1m-cao/ml1m-bprmf-pretrained.ckpt
+fi
+
+#TransUP
+if no_exist "../results/ml1m-cao/ml1m-transup-*.log"
+then
+    CUDA_VISIBLE_DEVICES=0 nohup python run_item_recommendation.py -data_path ~/git/datasets/ml1m-cao/ -log_path ~/git/results/ml1m-cao/ -rec_test_files valid.dat:test.dat -l2_lambda 1e-5 -negtive_samples 1 -model_type transup -nohas_visualization -dataset ml1m -batch_size 256 -embedding_size 100 -learning_rate 0.1 -topn 10 -seed 3 -eval_interval_steps 500 -training_steps 50000 -early_stopping_steps_to_wait 2500 -optimizer_type Adagrad -L1_flag -num_preferences 20 -nouse_st_gumbel -load_ckpt_file ~/git/results/ml1m-cao/ml1m-bprmf-pretrained1.ckpt &
+    BACK_PID=$!
+    wait $BACK_PID
+    mv ../results/ml1m-cao/ml1m-transup-*.ckpt_final ../results/ml1m-cao/ml1m-transup-pretrained.ckpt
 fi
 
 #CFKG (TransE) - Yongfeng Zhang, Qingyao Ai, Xu Chen, and Pengfei Wang. 2018. Learning over Knowledge-Base Embeddings for Recommendation. In SIGIR.
