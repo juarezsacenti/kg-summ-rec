3656bdf94a3147d629b63033a3d22081524000a7
juarez.sacenti <juarezsacenti@gmail.com>
2020-03-17 21:10:21 -0300

    run_summarized_sun

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ mkdir datasets/ml1m-sun/ml1m
 mkdir datasets/ml1m-cao2sun/ml1m
 mkdir datasets/ml1m-sun2cao/ml1m
 mkdir datasets/ml1m-sun2cao/ml1m/kg
+mkdir datasets/ml1m-summarized_sun/ml1m
+mkdir datasets/ml1m-summarized_sun/ml1m/kg
 
 git clone https://github.com/TaoMiner/joint-kg-recommender.git
 
@@ -17,3 +19,4 @@ cp Recurrent-Knowledge-Graph-Embedding/data/ml/* datasets/ml1m-sun/ml1m/
 mkdir results/
 mkdir results/ml1m-cao
 mkdir results/ml1m-sun
+mkdir results/ml1m-summarized_sun
