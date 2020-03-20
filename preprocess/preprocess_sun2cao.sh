bbe6ddefe273768d6914bf8ff364f702053593a3
juarez.sacenti <juarezsacenti@gmail.com>
2020-03-20 15:19:05 -0300

    Modified rkge call

--- preprocess/preprocess_sun2cao.sh
+++ preprocess/preprocess_sun2cao.sh
@@ -56,3 +56,18 @@ then
     BACK_PID=$!
     wait $BACK_PID
 fi
+
+#[activate rkge]
+conda deactivate
+conda activate rkge
+
+cd ../../Recurrent-Knowledge-Graph-Embedding
+
+#[rkge prepocessing]
+if no_exist "../../datasets/ml1m-sun2cao/ml1m/negative.txt" || no_exist "../../datasets/ml1m-sun2cao/positive-path.txt" || no_exist "../../datasets/ml1m-sun2cao/negative-path.txt"
+then
+    python auxiliary-mapping-ml.py --auxiliary ../datasets/ml1m-sun2cao/ml1m/clean_auxiliary.txt --mapping ../datasets/ml1m-sun2cao/ml1m/auxiliary-mapping.txt
+    python data-split.py --rating ../datasets/ml1m-sun/ml1m/rating-delete-missing-itemid.txt --train ../datasets/ml1m-sun2cao/ml1m/training.txt --test ../datasets/ml1m-sun2cao/ml1m/test.txt --ratio 0.8
+    python negative-sample.py --train ../datasets/ml1m-sun2cao/ml1m/training.txt --negative ../datasets/ml1m-sun2cao/ml1m/negative.txt --shrink 0.05
+    python path-extraction-ml.py --training ../datasets/ml1m-sun2cao/ml1m/training.txt --negtive ../datasets/ml1m-sun2cao/ml1m/negative.txt --auxiliary ../datasets/ml1m-sun2cao/ml1m/auxiliary-mapping.txt --positivepath ../datasets/ml1m-sun2cao/ml1m/positive-path.txt --negativepath ../datasets/ml1m-sun2cao/ml1m/negative-path.txt --pathlength 3 --samplesize 5
+fi
