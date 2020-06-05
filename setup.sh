004caf29590b7e420689880b49496d9eae9600fa
juarez.sacenti <juarezsacenti@gmail.com>
2020-06-05 01:05:18 -0300

    Adding CaseRecommender evaluator in Cao's algorithms;

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd ../
 
+################################################################################
+###                                 Datasets                                 ###
+################################################################################
 mkdir datasets
 mkdir datasets/ml1m-cao
 mkdir datasets/ml1m-cao/ml1m
@@ -25,11 +28,23 @@ mkdir datasets/ml1m-sun_sum4
 mkdir datasets/ml1m-sun_sum4/ml1m
 mkdir datasets/ml1m-sun_sum4/ml1m/kg
 
+################################################################################
+###                                 Projects                                 ###
+################################################################################
 git clone https://github.com/TaoMiner/joint-kg-recommender.git
-git clone https://github.com/juarezsacenti/ORBS.git
+mv joint-kg-recommender/jTransUp/models/item_recommendation.py joint-kg-recommender/jTransUp/models/item_recommendation.py_ORIG
+ln -sf know-rec/adapt/item_recommendation.py joint-kg-recommender/jTransUp/models/item_recommendation.py
+mv joint-kg-recommender/jTransUp/models/knowledgable_recommendation.py joint-kg-recommender/jTransUp/models/knowledgable_recommendation.py_ORIG
+ln -sf know-rec/adapt/knowledgable_recommendation.py joint-kg-recommender/jTransUp/models/knowledgable_recommendation.py
+
 git clone https://github.com/sunzhuntu/Recurrent-Knowledge-Graph-Embedding.git
 cp Recurrent-Knowledge-Graph-Embedding/data/ml/* datasets/ml1m-sun/ml1m/
 
+# git clone https://github.com/juarezsacenti/ORBS.git
+
+################################################################################
+###                                 Results                                  ###
+################################################################################
 mkdir results/
 mkdir results/ml1m-cao
 mkdir results/ml1m-sun
