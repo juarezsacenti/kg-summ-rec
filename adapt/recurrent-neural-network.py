c338ae21ff1f050ba4a648d6a2d0fc0969b615a6
juarez.sacenti <juarezsacenti@gmail.com>
2020-06-10 00:16:25 -0300

    Try 06: CaseRecommender evaluator in RKGE - case_rec_evaluate

--- adapt/recurrent-neural-network.py
+++ adapt/recurrent-neural-network.py
@@ -253,7 +253,7 @@ if __name__ == '__main__':
 	duration = end_time - start_time
 	print('the duration for loading item embedding is ' + str(duration) + '\n')
 
-    predictions_file = str(results_file) + "_old"
+    predictions_file = "data/ml/pred.txt"
     print(predictions_file)
     case_rec_evaluator(test_file, predictions_file, top_score_dict)
 
