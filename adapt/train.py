2978db2a410b50eddf29dcfd7f7d1dd445d1388a
juarez.sacenti <juarezsacenti@gmail.com>
2020-06-30 12:24:17 -0300

    Try 02: CaseRecommender evaluation in RippleNet

--- adapt/train.py
+++ adapt/train.py
@@ -2,6 +2,49 @@ import tensorflow as tf
 import numpy as np
 from model import RippleNet
 
+### +Added Imports
+from preprocess import load_dict
+from caserec.utils.process_data import ReadFile, WriteFile
+from caserec.evaluation.item_recommendation import ItemRecommendationEvaluation
+### -Added Imports
+
+### +Added Functions
+def case_rec_evaluation(sess, args, model, data, ripple_set, batch_size):
+    predictions_output_filepath = './ripplenet_pred.dat'
+    test_path = './ripplenet_test.dat'
+
+    i_map = load_dict('../data/' + args.dataset + '/i_map.txt')
+    u_map = load_dict('../data/' + args.dataset + '/i_map.txt')
+
+    start = 0
+    print_preds = []
+    while start < data.shape[0]:
+        feed_dict = get_feed_dict(args, model, data, ripple_set, start, start + batch_size)
+        labels, scores = sess.run([model.labels, model.scores_normalized], feed_dict)
+        for u, u_scores in enumerate(scores):
+            print('%d\t:%s'%u,str(u_scores))
+            for i, score in enumerate(u_scores):
+                print_preds.append((u_map[start+u], i_map[i], score))
+        start += batch_size
+    WriteFile(predictions_output_filepath, data=print_preds, sep='\t').write()
+
+    for u, u_data in enumerate(data):
+        for i, score in enumerate(u_data):
+            print_preds.append((u_map[start+u], i_map[i], score))
+
+    # Using CaseRecommender ReadFile class to read test_set from file
+    eval_data = ReadFile(input_file=test_path).read()
+    predictions_data = ReadFile(input_file=predictions_output_filepath).read()
+
+    # Creating CaseRecommender evaluator with item-recommendation parameters
+    evaluator = ItemRecommendationEvaluation(n_ranks=[10])
+
+    # Getting evaluation
+    item_rec_metrics = evaluator.evaluate(predictions_data['feedback'], eval_data)
+    print ('\nItem Recommendation Metrics:\n', item_rec_metrics)
+
+    return item_rec_metrics
+### -Added Functions
 
 def train(args, data_info, show_loss):
     train_data = data_info[0]
@@ -34,6 +77,10 @@ def train(args, data_info, show_loss):
             print('epoch %d    train auc: %.4f  acc: %.4f    eval auc: %.4f  acc: %.4f    test auc: %.4f  acc: %.4f'
                   % (step, train_auc, train_acc, eval_auc, eval_acc, test_auc, test_acc))
 
+### +Added Instructions
+        case_rec_evaluation(sess, args, model, test_data, ripple_set, args.batch_size)
+### -Added Instructions
+
 
 def get_feed_dict(args, model, data, ripple_set, start, end):
     feed_dict = dict()
@@ -56,29 +103,3 @@ def evaluation(sess, args, model, data, ripple_set, batch_size):
         acc_list.append(acc)
         start += batch_size
     return float(np.mean(auc_list)), float(np.mean(acc_list))
-
-
-def case_rec_evaluation(sess, args, model, data, ripple_set, batch_size):
-    start = 0
-    auc_list = []
-    acc_list = []
-    while start < data.shape[0]:
-        feed_dict = get_feed_dict(args, model, data, ripple_set, start, start + batch_size)
-        labels, scores = sess.run([self.labels, self.scores_normalized], feed_dict)
-        start += batch_size
-    predictions_output_filepath = './ripplenet_pred.dat'
-    WriteFile(predictions_output_filepath, data=print_list, sep='\t').write()
-
-    # Using CaseRecommender ReadFile class to read test_set from file
-    test_path = './ripplenet_test.dat'
-    eval_data = ReadFile(input_file=test_path).read()
-    predictions_data = ReadFile(input_file=predictions_output_filepath).read()
-
-    # Creating CaseRecommender evaluator with item-recommendation parameters
-    evaluator = ItemRecommendationEvaluation(n_ranks=[10])
-
-    # Getting evaluation
-    item_rec_metrics = evaluator.evaluate(predictions_data['feedback'], eval_data)
-    print ('\nItem Recommendation Metrics:\n', item_rec_metrics)
-
-    return item_rec_metrics
