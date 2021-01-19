64ea75356b48467e2188fcb30c2d6957718671a9
juarez.sacenti <juarezsacenti@gmail.com>
2021-01-19 18:43:46 -0300

    Bug in run_experiment1-kg_type for mv clustering (going back to multiview mode)

--- util/clean_kge-k-means.sh
+++ util/clean_kge-k-means.sh
@@ -3,12 +3,8 @@
 #[kge-k-means_data]
 clean_kge-k-means() {
     if [ -f "$HOME/git/kg-summ-rec/docker/kge-k-means_data/application.log" ]
-    then 
+    then
         yes | rm ~/git/kg-summ-rec/docker/kge-k-means_data/application.log
     fi
-
-    if ls "$HOME/git/kg-summ-rec/docker/kge-k-means_data/temp/*.*" 1> /dev/null 2>&1
-    then
-        yes | rm -r ~/git/kg-summ-rec/docker/kge-k-means_data/temp/*.*
-    fi
+    yes | rm -r ~/git/kg-summ-rec/docker/kge-k-means_data/temp/*.*
 }
