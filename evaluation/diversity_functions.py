b58af5c4c16992b5ec1bd4aad29b8f9f143e2ca9
juarez.sacenti <juarezsacenti@gmail.com>
2021-02-05 01:52:22 -0300

    Add IDL_genre metric

--- evaluation/diversity_functions.py
+++ evaluation/diversity_functions.py
@@ -82,3 +82,66 @@ def genre_redundancy_at_k(recommended_items, i2genre_map, k):
     assert genre_redundancy >= 0 and genre_redundancy <= 1, f'{genre_redundancy} = {total_distinct_genres} / {total_redundant_genres} {nl} {distinct_genres}'
 
     return genre_redundancy
+
+
+def jaccard_similarity_genre(genres_i, genres_j):
+    """
+    Intra-List Diversity
+    Ref: http://ceur-ws.org/Vol-2225/paper2.pdf, https://link.springer.com/article/10.1007/s10115-019-01371-0?shared-article-renderer
+
+    :param recommended_items: item id list in rank order (first element is the first item)
+    :type ranking: list
+
+    :param i2genre_map: Dictionary with item-genre map information. # To be filled as: {item_id: [genre_id_1, genre_id_2, ..., genre_id_N]} and also {'distinct_genres': [genre_id_1, genre_id_2, ..., genre_id_N]}
+    :type i2genre_map: dict
+
+    :param k: length of recommended list
+    :type k: int
+
+    :return: Genre coverage @ k
+    :rtype: float
+
+    """
+
+    fst, snd = (genres_i, genres_j) if len(genres_i) < len(genres_j) else (genres_j, genres_i)
+    genres_intersection = set(fst).intersection(snd)
+    genres_union = set(genres_i.extend(genres_j))
+    return len(genres_intersection) / len(genres_union)
+
+
+def intra_list_diversity_genre(recommended_items, i2genre_map, k):
+    """
+    Intra-List Diversity
+    Ref: http://ceur-ws.org/Vol-2225/paper2.pdf, https://link.springer.com/article/10.1007/s10115-019-01371-0?shared-article-renderer
+
+    :param recommended_items: item id list in rank order (first element is the first item)
+    :type ranking: list
+
+    :param i2genre_map: Dictionary with item-genre map information. # To be filled as: {item_id: [genre_id_1, genre_id_2, ..., genre_id_N]} and also {'distinct_genres': [genre_id_1, genre_id_2, ..., genre_id_N]}
+    :type i2genre_map: dict
+
+    :param k: length of recommended list
+    :type k: int
+
+    :return: Genre coverage @ k
+    :rtype: float
+
+    """
+
+    assert k >= 1
+    if len(recommended_items) != k:
+        raise ValueError('Recommended item list length < k')
+
+    sorted_predictions = sorted(recommended_items)
+    sum = 0
+    for i in sorted_predictions:
+        for j in sorted_predictions:
+            if i < j:
+                genres_i = sorted(i2genre_map.get(str(i),[]))
+                genres_j = sorted(i2genre_map.get(str(j),[]))
+                sum += (1 - jaccard_similarity_genre(genres_i, genres_j))
+            else:
+                pass
+
+    diversity = (sum * 2) / (k * (k - 1))
+    return genre_redundancy
