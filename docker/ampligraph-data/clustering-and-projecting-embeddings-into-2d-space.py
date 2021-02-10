eff98e2792da18c2f31a84efff35db0cb967fb9b
juarez.sacenti <juarezsacenti@gmail.com>
2021-02-10 20:07:04 -0300

    Add ampligraph example

---
+++ docker/ampligraph-data/clustering-and-projecting-embeddings-into-2d-space.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+import requests
+
+from ampligraph.datasets import load_from_csv
+from ampligraph.latent_features import ComplEx
+from ampligraph.evaluation import evaluate_performance
+from ampligraph.evaluation import mr_score, mrr_score, hits_at_n_score
+from ampligraph.evaluation import train_test_split_no_unseen
+
+# International football matches triples
+url = 'https://ampligraph.s3-eu-west-1.amazonaws.com/datasets/football.csv'
+open('football.csv', 'wb').write(requests.get(url).content)
+X = load_from_csv('.', 'football.csv', sep=',')[:, 1:]
+
+# Train test split
+X_train, X_test = train_test_split_no_unseen(X, test_size=10000)
+
+# ComplEx model
+model = ComplEx(batches_count=50,
+                epochs=300,
+                k=100,
+                eta=20,
+                optimizer='adam',
+                optimizer_params={'lr':1e-4},
+                loss='multiclass_nll',
+                regularizer='LP',
+                regularizer_params={'p':3, 'lambda':1e-5},
+                seed=0,
+                verbose=True)
+
+model.fit(X_train)
+
+filter_triples = np.concatenate((X_train, X_test))
+ranks = evaluate_performance(X_test,
+                             model=model,
+                             filter_triples=filter_triples,
+                             use_default_protocol=True,
+                             verbose=True)
+
+mr = mr_score(ranks)
+mrr = mrr_score(ranks)
+
+print("MRR: %.2f" % (mrr))
+print("MR: %.2f" % (mr))
+
+hits_10 = hits_at_n_score(ranks, n=10)
+print("Hits@10: %.2f" % (hits_10))
+hits_3 = hits_at_n_score(ranks, n=3)
+print("Hits@3: %.2f" % (hits_3))
+hits_1 = hits_at_n_score(ranks, n=1)
+print("Hits@1: %.2f" % (hits_1))
+'''
+MRR: 0.25
+MR: 4927.33
+Hits@10: 0.35
+Hits@3: 0.28
+Hits@1: 0.19
+'''
+
+import re
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+from adjustText import adjust_text
+from incf.countryutils import transformations
+from ampligraph.discovery import find_clusters
+
+# Get the teams entities and their corresponding embeddings
+triples_df = pd.DataFrame(X, columns=['s', 'p', 'o'])
+teams = triples_df.s[triples_df.s.str.startswith('Team')].unique()
+team_embeddings = dict(zip(teams, model.get_embeddings(teams)))
+team_embeddings_array = np.array([i for i in team_embeddings.values()])
+
+# Project embeddings into 2D space via PCA
+embeddings_2d = PCA(n_components=2).fit_transform(team_embeddings_array)
+
+# Cluster embeddings (on the original space)
+clustering_algorithm = KMeans(n_clusters=6, n_init=100, max_iter=500, random_state=0)
+clusters = find_clusters(teams, model, clustering_algorithm, mode='entity')
+
+# This function maps country to continent
+def cn_to_ctn(country):
+    try:
+        original_name = ' '.join(re.findall('[A-Z][^A-Z]*', country[4:]))
+        return transformations.cn_to_ctn(original_name)
+    except KeyError:
+        return "unk"
+
+plot_df = pd.DataFrame({"teams": teams,
+                        "embedding1": embeddings_2d[:, 0],
+                        "embedding2": embeddings_2d[:, 1],
+                        "continent": pd.Series(teams).apply(cn_to_ctn),
+                        "cluster": "cluster" + pd.Series(clusters).astype(str)})
+
+# Top 20 teams in 2019 according to FIFA rankings
+top20teams = ["TeamBelgium", "TeamFrance", "TeamBrazil", "TeamEngland", "TeamPortugal",
+              "TeamCroatia", "TeamSpain", "TeamUruguay", "TeamSwitzerland", "TeamDenmark",
+              "TeamArgentina", "TeamGermany", "TeamColombia", "TeamItaly", "TeamNetherlands",
+              "TeamChile", "TeamSweden", "TeamMexico", "TeamPoland", "TeamIran"]
+
+np.random.seed(0)
+
+# Plot 2D embeddings with country labels
+def plot_clusters(hue):
+    plt.figure(figsize=(12, 12))
+    plt.title("{} embeddings".format(hue).capitalize())
+    ax = sns.scatterplot(data=plot_df[plot_df.continent!="unk"],
+                         x="embedding1", y="embedding2", hue=hue)
+    texts = []
+    for i, point in plot_df.iterrows():
+        if point["teams"] in top20teams or np.random.random() < 0.1:
+            texts.append(plt.text(point['embedding1']+0.02,
+                         point['embedding2']+0.01,
+                         str(point["teams"])))
+    adjust_text(texts)
+
+plot_clusters("continent")
+plt.savefig('./temp/continent.png')
+plot_clusters("cluster")
+plt.savefig('./temp/cluster.png')
