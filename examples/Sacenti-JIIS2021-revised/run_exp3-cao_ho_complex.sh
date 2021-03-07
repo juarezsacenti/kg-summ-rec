49b1d0e6ee45a132d28a2b3f11423efdf59eb87b
juarez.sacenti <juarezsacenti@gmail.com>
2021-03-07 04:28:25 -0300

    Add JIIS-revised-exp3 Minor fix in JIIS-revised-exp1

--- examples/Sacenti-JIIS2021-revised/run_exp3-cao_ho_complex.sh
+++ examples/Sacenti-JIIS2021-revised/run_exp3-cao_ho_complex.sh
@@ -18,7 +18,7 @@
 
 #######################################
 # Run experiment kg_type for Sacenti 2021 - JOURNAL.
-# - Datasets: ml-sun
+# - Datasets: ml-cao
 # - Split: hold-out (ho)
 # - Filtering: infrequent entities filtering at 0 (sKG) and at 10 (sfKG)
 # - KG Summarization:
@@ -35,15 +35,15 @@
 #   HOME
 #   experiment: running experiment. Default is 'Sacenti-JIIS2021-revised'.
 # FUNCTIONS:
-#   preprocess_sun_oKG
-#   preprocess_sun_fKG
-#   summarize_sun_sKG
-#   summarize_sun_sfKG
-#   recommend_sun_sKG
-#   recommend_sun_sfKG
+#   preprocess_cao_oKG
+#   preprocess_cao_fKG
+#   summarize_cao_sKG
+#   summarize_cao_sfKG
+#   recommend_cao_sKG
+#   recommend_cao_sfKG
 #   run_experiments
 #######################################
-experiment='Sacenti-JIIS2021-revised-ho'
+experiment='JIIS-revised-exp3'
 seed=0
 verbose=false
 overall_comp_cost="$HOME/git/results/${experiment}/overall_comp_cost.tsv"
@@ -57,11 +57,11 @@ overall_comp_cost="$HOME/git/results/${experiment}/overall_comp_cost.tsv"
 source $HOME/git/kg-summ-rec/util/util.sh
 
 #######################################
-# Import preprocess/cao-format_ml-sun.sh
+# Import preprocess/cao-format_ml-cao.sh
 # FUNCTIONS:
-#   cao-format_ml-sun 'dataset' 'low_frequence'
+#   cao-format_ml-cao 'dataset' 'low_frequence'
 #######################################
-source $HOME/git/kg-summ-rec/preprocess/cao-format_ml-sun.sh
+source $HOME/git/kg-summ-rec/preprocess/cao-format_ml-cao.sh
 
 #######################################
 # Import ../summarization/kge-k-means.sh
@@ -96,68 +96,68 @@ source $HOME/git/kg-summ-rec/util/comp_cost.sh
 ####
 # KG preprocessing
 #
-# - Datasets: ml-sun
+# - Datasets: ml-cao, ml-cao
 # - Split: hold-out (ho)
 # - Filtering: infrequent entities filtering at 0 (oKG) and at 10 (fKG)
 ####
-preprocess_sun_oKG() {
-    if [ ! -d "$HOME/git/datasets/${experiment}/ml-sun_ho_oKG" ]
+preprocess_cao_oKG() {
+    if [ ! -d "$HOME/git/datasets/${experiment}/ml-cao_ho_oKG" ]
     then
         local STARTTIME=$(date +%s)
-        # Create folders for Sun's original KG (oKG)
-        if no_exist "$HOME/git/datasets/${experiment}/ml-sun_ho_oKG"
+        # Create folders for cao's original KG (oKG)
+        if no_exist "$HOME/git/datasets/${experiment}/ml-cao_ho_oKG"
         then
-            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_sun_oKG: Creating ~/git/datasets/${experiment}/ml-sun_ho_oKG"; fi
+            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_cao_oKG: Creating ~/git/datasets/${experiment}/ml-cao_ho_oKG"; fi
             cd $HOME/git/kg-summ-rec/util
-            copy_ml_sun "$HOME/git/datasets/ml-sun" "$HOME/git/datasets/${experiment}/ml-sun_ho_oKG"
+            copy_ml_cao "$HOME/git/datasets/ml-cao" "$HOME/git/datasets/${experiment}/ml-cao_ho_oKG"
             cd $HOME/git/kg-summ-rec
         fi
 
-        # Preprocess oKG
+        Preprocess oKG
         cd $HOME/git/kg-summ-rec/preprocess
         LOW_FREQUENCE=0    #Low Frequence Filtering (0, 10)
         if [ "$verbose" = true ]
         then
-            cao-format_ml-sun "ml-sun_ho_oKG" ${LOW_FREQUENCE} 'ho' ${seed} 'true'
+            cao-format_ml-cao "ml-cao_ho_oKG" ${LOW_FREQUENCE} ${seed} 'true'
         else
-            cao-format_ml-sun "ml-sun_ho_oKG" ${LOW_FREQUENCE} 'ho' ${seed} 'false'
+            cao-format_ml-cao "ml-cao_ho_oKG" ${LOW_FREQUENCE} ${seed} 'false'
         fi
         cd $HOME/git/kg-summ-rec
 
         # Collect oKG statistics
-        if no_exist "$HOME/git/results/${experiment}/ml-sun_ho_oKG"
+        if no_exist "$HOME/git/results/${experiment}/ml-cao_ho_oKG"
         then
-            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_sun_oKG: Creating ~/git/results/${experiment}/ml-sun_ho_oKG"; fi
-            mkdir ~/git/results/${experiment}/ml-sun_ho_oKG
+            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_cao_oKG: Creating ~/git/results/${experiment}/ml-cao_ho_oKG"; fi
+            mkdir ~/git/results/${experiment}/ml-cao_ho_oKG
         fi
-        if no_exist "$HOME/git/results/${experiment}/ml-sun_ho_oKG/kg-ig_stats.tsv"
+        if no_exist "$HOME/git/results/${experiment}/ml-cao_ho_oKG/kg-ig_stats.tsv"
         then
-            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_sun_oKG: Creating ~/git/results/${experiment}/ml-sun_ho_oKG/kg-ig_stats.tsv"; fi
+            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_cao_oKG: Creating ~/git/results/${experiment}/ml-cao_ho_oKG/kg-ig_stats.tsv"; fi
             cd $HOME/git/kg-summ-rec/util
             conda deactivate
             conda activate kg-summ-rec
-            python kg2rdf.py --mode 'statistics' --kgpath "~/git/datasets/${experiment}/ml-sun_ho_oKG" \
-            --input "~/git/datasets/${experiment}/ml-sun_ho_oKG/kg-ig.nt" \
-            --output "~/git/results/${experiment}/ml-sun_ho_oKG/kg-ig_stats.tsv"
+            python kg2rdf.py --mode 'statistics' --kgpath "~/git/datasets/${experiment}/ml-cao_ho_oKG" \
+            --input "~/git/datasets/${experiment}/ml-cao_ho_oKG/kg-ig.nt" \
+            --output "~/git/results/${experiment}/ml-cao_ho_oKG/kg-ig_stats.tsv"
             cd $HOME/git/kg-summ-rec
         fi
 
         local ENDTIME=$(date +%s)
-        echo -e "preprocess_sun_oKG\t$(($ENDTIME - $STARTTIME))\t${STARTTIME}\t${ENDTIME}" >> ${overall_comp_cost}
+        echo -e "preprocess_cao_oKG\t$(($ENDTIME - $STARTTIME))\t${STARTTIME}\t${ENDTIME}" >> ${overall_comp_cost}
     fi
 }
 
-preprocess_sun_fKG() {
-    if [ ! -d "$HOME/git/datasets/${experiment}/ml-sun_ho_fKG" ]
+preprocess_cao_fKG() {
+    if [ ! -d "$HOME/git/datasets/${experiment}/ml-cao_ho_fKG" ]
     then
         local STARTTIME=$(date +%s)
 
-        # Create folders for Sun's filtered KG (fKG)
-        if no_exist "$HOME/git/datasets/${experiment}/ml-sun_ho_fKG"
+        # Create folders for cao's filtered KG (fKG)
+        if no_exist "$HOME/git/datasets/${experiment}/ml-cao_ho_fKG"
         then
-            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_sun_fKG: Creating ~/git/datasets/${experiment}/ml-sun_ho_fKG"; fi
+            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_cao_fKG: Creating ~/git/datasets/${experiment}/ml-cao_ho_fKG"; fi
             cd $HOME/git/kg-summ-rec/util
-            copy_ml_sun "$HOME/git/datasets/ml-sun" "$HOME/git/datasets/${experiment}/ml-sun_ho_fKG"
+            copy_ml_cao "$HOME/git/datasets/ml-cao" "$HOME/git/datasets/${experiment}/ml-cao_ho_fKG"
             cd $HOME/git/kg-summ-rec
         fi
 
@@ -166,54 +166,54 @@ preprocess_sun_fKG() {
         LOW_FREQUENCE=10    #Low Frequence Filtering (0, 10)
         if [ "$verbose" = true ]
         then
-            cao-format_ml-sun "ml-sun_ho_fKG" ${LOW_FREQUENCE} 'ho' ${seed} 'true'
+            cao-format_ml-cao "ml-cao_ho_fKG" ${LOW_FREQUENCE} ${seed} 'true'
         else
-            cao-format_ml-sun "ml-sun_ho_fKG" ${LOW_FREQUENCE} 'ho' ${seed} 'false'
+            cao-format_ml-cao "ml-cao_ho_fKG" ${LOW_FREQUENCE} ${seed} 'false'
         fi
         cd $HOME/git/kg-summ-rec
 
         # Collect oKG statistics
-        if no_exist "$HOME/git/results/${experiment}/ml-sun_ho_fKG"
+        if no_exist "$HOME/git/results/${experiment}/ml-cao_ho_fKG"
         then
-            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_sun_fKG: Creating ~/git/results/${experiment}/ml-sun_ho_fKG"; fi
-            mkdir ~/git/results/${experiment}/ml-sun_ho_fKG
+            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_cao_fKG: Creating ~/git/results/${experiment}/ml-cao_ho_fKG"; fi
+            mkdir ~/git/results/${experiment}/ml-cao_ho_fKG
         fi
-        if no_exist "$HOME/git/results/${experiment}/ml-sun_ho_fKG/kg-ig_stats.tsv"
+        if no_exist "$HOME/git/results/${experiment}/ml-cao_ho_fKG/kg-ig_stats.tsv"
         then
-            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_sun_fKG: Creating ~/git/results/${experiment}/ml-sun_ho_fKG/kg-ig_stats.tsv"; fi
+            if [ "$verbose" = true ]; then echo "[kg-summ-rec] preprocess_cao_fKG: Creating ~/git/results/${experiment}/ml-cao_ho_fKG/kg-ig_stats.tsv"; fi
             cd $HOME/git/kg-summ-rec/util
             conda deactivate
             conda activate kg-summ-rec
-            python kg2rdf.py --mode 'statistics' --kgpath "~/git/datasets/${experiment}/ml-sun_ho_fKG" \
-            --input "~/git/datasets/${experiment}/ml-sun_ho_fKG/kg-ig.nt" \
-            --output "~/git/results/${experiment}/ml-sun_ho_fKG/kg-ig_stats.tsv"
+            python kg2rdf.py --mode 'statistics' --kgpath "~/git/datasets/${experiment}/ml-cao_ho_fKG" \
+            --input "~/git/datasets/${experiment}/ml-cao_ho_fKG/kg-ig.nt" \
+            --output "~/git/results/${experiment}/ml-cao_ho_fKG/kg-ig_stats.tsv"
             cd $HOME/git/kg-summ-rec
         fi
 
         local ENDTIME=$(date +%s)
-        echo -e "preprocess_sun_fKG\t$(($ENDTIME - $STARTTIME))\t${STARTTIME}\t${ENDTIME}" >> ${overall_comp_cost}
+        echo -e "preprocess_cao_fKG\t$(($ENDTIME - $STARTTIME))\t${STARTTIME}\t${ENDTIME}" >> ${overall_comp_cost}
     fi
 }
 
 ####
 # KG summarization
 #
-# - Datasets: ml-sun
+# - Datasets: ml-cao, ml-cao
 # - Split: hold-out (ho)
 # - Filtering: infrequent entities filtering at 0 (sKG) and at 10 (sfKG)
 ####
-summarize_sun_sKG() {
+summarize_cao_sKG() {
     # KG summarization
-    kg_summarization 'ml-sun' 'ho' 'sKG'
+    kg_summarization 'ml-cao' 'ho' 'sKG'
 }
 
-summarize_sun_sfKG() {
+summarize_cao_sfKG() {
     # KG summarization
-    kg_summarization 'ml-sun' 'ho' 'sfKG'
+    kg_summarization 'ml-cao' 'ho' 'sfKG'
 }
 
 kg_summarization() {
-    local dataset=$1 # Input dataset: ml-sun
+    local dataset=$1 # Input dataset: ml-cao, ml-cao
     local split_mode=$2 # Split mode: hold-out (ho)
     local filtering=$3 # Filtering: infrequent entities filtering at 0 (sKG) and at 10 (sfKG)
 
@@ -246,8 +246,11 @@ summarize() {
     local learning_rate='0.005'
     local kg_filename="kg-${kg_type}.nt"
 
+    #clean_kge-k-means
+
     summ_modes=(sv mv)
-    summ_ratios=(25 50 75)
+    #summ_ratios=(25 50 75)
+    summ_ratios=(50)
     for summarization_mode in "${summ_modes[@]}"
     do
         for ratio in "${summ_ratios[@]}"
@@ -281,7 +284,8 @@ preprocess_summ() {
 
     summ_modes=(sv mv)
     summ_algos=(complex)
-    summ_rates=(25 50 75)
+    #summ_rates=(25 50 75)
+    summ_rates=(50)
     local STARTTIME=0
     local ENDTIME=0
     for m in "${summ_modes[@]}"
@@ -296,9 +300,9 @@ preprocess_summ() {
                     STARTTIME=$(date +%s)
                     if [ "$verbose" = true ]
                     then
-                        cao-format_summ "${dataset_in}" "${dirName}" "${low_frequence}" "${seed}" 'true'
+                        cao-format_summ "${dataset_in}" "${dirName}" "${low_frequence}" 'true'
                     else
-                        cao-format_summ "${dataset_in}" "${dirName}" "${low_frequence}" "${seed}" 'false'
+                        cao-format_summ "${dataset_in}" "${dirName}" "${low_frequence}" 'false'
                     fi
                     ENDTIME=$(date +%s)
                     echo -e "preprocess_summ-${dataset_out}_${kg_type}-${m}-${a}-${r}\t$(($ENDTIME - $STARTTIME))\t${STARTTIME}\t${ENDTIME}" >> ${overall_comp_cost}
@@ -322,7 +326,8 @@ measure_summ_impact() {
 
     summ_modes=(sv mv)
     summ_algos=(complex)
-    summ_rates=(25 50 75)
+    #summ_rates=(25 50 75)
+    summ_rates=(50)
     local STARTTIME=0
     local ENDTIME=0
     for m in "${summ_modes[@]}"
@@ -350,39 +355,32 @@ measure_summ_impact() {
 }
 
 ####
-# recommend_sun_sKG
+# recommend_cao_sKG
 #
-# - ml-sun_ho_oKG
+# - ml-cao_ho_oKG
 #
-# - ml-sun_ho_sKG_complex-ig-sv-75, ml-sun_ho_sKG_complex-ig-sv-50, ml-sun_ho_sKG_complex-ig-sv-25
-# - ml-sun_ho_sKG_complex-ig-mv-75, ml-sun_ho_sKG_complex-ig-mv-50, ml-sun_ho_sKG_complex-ig-mv-25
+# - ml-cao_ho_sKG_complex-ig-sv-75, ml-cao_ho_sKG_complex-ig-sv-50, ml-cao_ho_sKG_complex-ig-sv-25
+# - ml-cao_ho_sKG_complex-ig-mv-75, ml-cao_ho_sKG_complex-ig-mv-50, ml-cao_ho_sKG_complex-ig-mv-25
 ####
-recommend_sun_sKG() {
+recommend_cao_sKG() {
     # kg-summ-rec/evaluation
     if [[ ! $PYTHONPATH = *git/kg-summ-rec/evaluation* ]]
     then
         export PYTHONPATH="${HOME}/git/kg-summ-rec/evaluation:${PYTHONPATH}"
     fi
 
-    # original KG
-    if no_exist "$HOME/git/results/${experiment}/ml-sun_ho_oKG/*.log"
-    then
-        if [ "$verbose" = true ]; then echo "[kg-summ-rec] kg_recommendation: Creating ~/git/results/${experiment}/ml-sun_ho_oKG/*.log"; fi
-        recommend "ml-sun_ho_oKG" '4873,487300,24363' '2663,266300,13317' '266,26630,1331' '10392,1039200,51960' 256 0.005
-    fi
-
-    kg_recommendation "ml-sun_ho_oKG" "ml-sun_ho_sKG"
+    kg_recommendation "ml-cao_ho_oKG" "ml-cao_ho_sKG"
 }
 
 ####
-# recommend_sun_sfKG
+# recommend_cao_sfKG
 #
-# - ml-sun_ho_fKG
+# - ml-cao_ho_fKG
 #
-# - ml-sun_ho_sfKG_complex-ig-sv-75, ml-sun_ho_sfKG_complex-ig-sv-50, ml-sun_ho_sfKG_complex-ig-sv-25
-# - ml-sun_ho_sfKG_complex-ig-mv-75, ml-sun_ho_sfKG_complex-ig-mv-50, ml-sun_ho_sfKG_complex-ig-mv-25
+# - ml-cao_ho_sfKG_complex-ig-sv-75, ml-cao_ho_sfKG_complex-ig-sv-50, ml-cao_ho_sfKG_complex-ig-sv-25
+# - ml-cao_ho_sfKG_complex-ig-mv-75, ml-cao_ho_sfKG_complex-ig-mv-50, ml-cao_ho_sfKG_complex-ig-mv-25
 ####
-recommend_sun_sfKG() {
+recommend_cao_sfKG() {
     # kg-summ-rec/evaluation
     if [[ ! $PYTHONPATH = *git/kg-summ-rec/evaluation* ]]
     then
@@ -393,20 +391,29 @@ recommend_sun_sfKG() {
     if no_exist "$HOME/git/results/${experiment}/ml-sun_ho_fKG/*.log"
     then
         if [ "$verbose" = true ]; then echo "[kg-summ-rec] kg_recommendation: Creating ~/git/results/${experiment}/ml-sun_ho_fKG/*.log"; fi
-        recommend "ml-sun_ho_fKG" '4873,487300,24363' '2663,266300,13317' '266,26630,1331' '10392,1039200,51960' 256 0.005
+        recommend "ml-cao_ho_fKG" '4873,487300,24363' '2663,266300,13317' '266,26630,1331' '10392,1039200,51960' 256 0.005
     fi
 
-    kg_recommendation "ml-sun_ho_oKG" "ml-sun_ho_sfKG"
+    kg_recommendation "ml-cao_ho_oKG" "ml-cao_ho_sfKG"
 }
 
 kg_recommendation() {
     local dataset_in=$1
     local dataset_out=$2
 
+    # original KG
+    if no_exist "$HOME/git/results/${experiment}/${dataset_in}/*.log"
+    then
+        if [ "$verbose" = true ]; then echo "[kg-summ-rec] kg_recommendation: Creating ~/git/results/${experiment}/${dataset_in}/*.log"; fi
+        #recommend "${dataset_in}" '4873,487300,24363' '2663,266300,13317' '266,26630,1331' '10392,1039200,51960' 256 0.005
+        recommend "${dataset_in}" '520,13000,13026' '4940,123500,123747' '4940,123500,123747' '10940,273500,274047' 256 0.005
+    fi
+
     summ_algos=(complex)
     #summ_types=(ig uig euig)
     summ_types=(ig)
-    summ_ratios=(25 50 75)
+    #summ_ratios=(25 50 75)
+    summ_ratios=(50)
     summ_modes=(sv mv)
     for a in "${summ_algos[@]}"
     do
@@ -417,12 +424,8 @@ kg_recommendation() {
                 for m in "${summ_modes[@]}"
                 do
                     local dirName="${dataset_out}_${t}-${m}-${a}-${r}"
-
-                    cp ~/git/results/$experiment/${dataset_in}/ml1m-bprmf-pretrained.ckpt ~/git/results/$experiment/${dirName}/ml1m-bprmf-pretrained.ckpt
-                    cp ~/git/results/$experiment/${dataset_in}/ml1m-transup-pretrained.ckpt ~/git/results/$experiment/${dirName}/ml1m-transup-pretrained.ckpt
-
                     if [ "$verbose" = true ]; then echo "[kg-summ-rec] kg_recommendation: Creating ~/git/results/${experiment}/${dirName}/*.log"; fi
-                    recommend "${dirName}" '4873,487300,24363' '2663,266300,13317' '266,26630,1331' '10392,1039200,51960' 256 0.005
+                    recommend "${dirName}" '9150,915000,45750' '500,50000,2500' '5000,500000,25000' '19520,1952000,97600' 256 0.005
                 done
             done
         done
@@ -492,7 +495,7 @@ recommend() {
     then
         STARTTIME=$(date +%s)
         if [ "$verbose" = true ]; then echo "[kg-summ-rec] recommend: Running TransUP with $DATASET"; fi
-        CUDA_VISIBLE_DEVICES=0 nohup python run_item_recommendation.py -data_path ~/git/datasets/$experiment/$DATASET/cao-format/ -log_path ~/git/results/$experiment/$DATASET/ -rec_test_files valid.dat:test.dat -l2_lambda 1e-5 -negtive_samples 1 -model_type transup -nohas_visualization -dataset ml1m -batch_size ${BATCH_SIZE} -embedding_size 100 -learning_rate ${LEARNING_RATE} -topn 10 -seed ${seed} -eval_interval_steps ${TUP_EPOCHS[0]} -training_steps ${TUP_EPOCHS[1]} -early_stopping_steps_to_wait ${TUP_EPOCHS[2]} -optimizer_type Adagrad -L1_flag -num_preferences 3 -nouse_st_gumbel -load_ckpt_file "$HOME/git/results/$experiment/$DATASET/ml1m-bprmf-pretrained.ckpt" & # ml-sun num_preferences = 3
+        CUDA_VISIBLE_DEVICES=0 nohup python run_item_recommendation.py -data_path ~/git/datasets/$experiment/$DATASET/cao-format/ -log_path ~/git/results/$experiment/$DATASET/ -rec_test_files valid.dat:test.dat -l2_lambda 1e-5 -negtive_samples 1 -model_type transup -nohas_visualization -dataset ml1m -batch_size ${BATCH_SIZE} -embedding_size 100 -learning_rate ${LEARNING_RATE} -topn 10 -seed ${seed} -eval_interval_steps ${TUP_EPOCHS[0]} -training_steps ${TUP_EPOCHS[1]} -early_stopping_steps_to_wait ${TUP_EPOCHS[2]} -optimizer_type Adagrad -L1_flag -num_preferences 3 -nouse_st_gumbel -load_ckpt_file "$HOME/git/results/$experiment/$DATASET/ml1m-bprmf-pretrained.ckpt" & # ml-cao num_preferences = 3
         wait $!
         mv ~/git/results/$experiment/$DATASET/ml1m-transup-*.ckpt ~/git/results/$experiment/$DATASET/ml1m-transup-pretrained.ckpt
         ENDTIME=$(date +%s)
@@ -566,19 +569,19 @@ run_experiment() {
         touch ${overall_comp_cost}
     fi
 
+    clean_kge-k-means
+
     # Preprocessing
-    preprocess_sun_oKG
-    preprocess_sun_fKG
+    preprocess_cao_oKG
+    #preprocess_cao_fKG
 
     # Summarization
-    clean_kge-k-means
-    summarize_sun_sKG
-    clean_kge-k-means
-    summarize_sun_sfKG
+    summarize_cao_sKG
+    #summarize_cao_sfKG
 
     # Recommendation
-    recommend_sun_sKG
-    recommend_sun_sfKG
+    recommend_cao_sKG
+    #recommend_cao_sfKG
 }
 run_experiment $1 $2 $3
-#bash -i examples/Sacenti-JIIS2021-revised/run_exp1-sun_ho_complex.sh "JIIS-revised-exp1" 0 'false' |& tee out-revised-exp1-5.txt
+#bash -i examples/Sacenti-JIIS2021-revised/run_exp3-cao_ho_complex.sh "JIIS-revised-exp3" 0 'false' |& tee out-revised-exp3-1.txt
